491fb5a7d7170cb868325762ebc373b4a5594d06
zhangxiaojia <zhangxiaojia@baidu.com>
2017-03-18 09:20:45 +0800

    add lifecycle log

--- app/src/main/java/com/example/zhangxiaojia/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/zhangxiaojia/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.example.zhangxiaojia.geoquiz;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -9,6 +10,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "QuizActivity";
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -48,6 +51,7 @@ public class QuizActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = (TextView) findViewById(R.id.quesiont_text_view);
@@ -58,7 +62,7 @@ public class QuizActivity extends AppCompatActivity implements View.OnClickListe
 
         mFalseButton = (Button) findViewById(R.id.false_button);
         mFalseButton.setOnClickListener(this);
-
+        
         mPrevButton = (ImageButton) findViewById(R.id.prev_button);
         mPrevButton.setOnClickListener(this);
 
@@ -66,6 +70,36 @@ public class QuizActivity extends AppCompatActivity implements View.OnClickListe
         mNextButton.setOnClickListener(this);
 
         updateQuestion();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 
     @Override
